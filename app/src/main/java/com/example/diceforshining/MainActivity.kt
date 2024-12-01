63bea48c7fbeb66f4a83f0bcda165f79e4956394
WindNi <nituowei@gmail.com>
2024-12-01 21:14:23 +0800

    version:2.0 Changes:Show dice number with it's dice image.

--- app/src/main/java/com/example/diceforshining/MainActivity.kt
+++ app/src/main/java/com/example/diceforshining/MainActivity.kt
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint
 import android.media.AudioManager
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -51,6 +52,41 @@ class MainActivity : AppCompatActivity() {
         val diceRoll = dice.roll()
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
+
+        val luckyTextView: TextView = findViewById(R.id.textView2)
+        val luckyNumber = 6
+        if (luckyNumber == diceRoll) {
+            luckyTextView.text = "Lucky Number!"
+        }else{
+            luckyTextView.text = ""
+        }
+
+        val diceImage: ImageView = findViewById(R.id.imageView)
+/*        if (diceRoll == 1){
+            diceImage.setImageResource(R.drawable.dice_1)
+        }else if (diceRoll == 2){
+            diceImage.setImageResource(R.drawable.dice_2)
+        }*/
+        //comment the if code block above since it's better to use when here
+/*        when(diceRoll){
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }*/
+        //But there is still another better way
+        val drawableResource = when(diceRoll){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+
     }
 }
 
